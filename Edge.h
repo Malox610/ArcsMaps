752ec0e4520a19c9a29c16278b155de46937adf5
ESPEDUZA <75570746+ESPEDUZA@users.noreply.github.com>
2021-04-06 23:44:16 +0200

    Update Edge.h

--- Edge.h
+++ Edge.h
@@ -1,6 +1,6 @@
 #ifndef EDGE_H_INCLUDED
 #define EDGE_H_INCLUDED
-
+#include "string"
 class Edge{
 
 private :
@@ -8,12 +8,14 @@ private :
     int m_dest;
     int m_weight;
     int m_source;
+    std::string m_type;
+    std::string m_nom;
 
 public:
 
 
 
-      Edge(int _source, int _dest, int _weight);
+      Edge(int _source, int _dest, int _weight, std::string _type);
       int getSource ()const;
       int getDest ()const;
       int getWeight ()const;
