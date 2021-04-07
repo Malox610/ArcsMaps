7fde587c9fa2c167e1614496eb258e41dce19756
ESPEDUZA <75570746+ESPEDUZA@users.noreply.github.com>
2021-04-07 16:07:50 +0200

    Update Edge.cpp

--- Edge.cpp
+++ Edge.cpp
@@ -1,15 +1,21 @@
 #include "Edge.h"
+#include "Node.h"
 #include <iostream>
 
-Edge::Edge(int _source, int _dest, int _weight, std::string _type): m_source(_source), m_dest(_dest), m_weight(_weight), m_type(_type)
+Edge::Edge(Node _source, Node _dest, int _weight): m_source(_source), m_dest(_dest), m_weight(_weight)
 {}
 
-int Edge::getSource()const
+int Edge::getNum() const
+{
+    return m_num;
+}
+
+Node Edge::getSource()const
 {
     return m_source;
 }
 
-int Edge::getDest()const
+Node Edge::getDest()const
 {
     return m_dest;
 }
@@ -20,3 +26,43 @@ int Edge::getWeight()const
 }
 
 Edge::~Edge(){}
+
+std::string Edge::getType() const
+{
+    return m_type;
+}
+
+std::string Edge::getNom() const
+{
+    return m_nom;
+}
+
+void Edge::setWeight()
+{
+    m_weight = abs(m_dest.getWeight()- m_source.getWeight());
+}
+
+void Edge::setType(std::string newType)
+{
+    m_type = newType;
+}
+
+void Edge::setNom(std::string newNom)
+{
+    m_nom = newNom;
+}
+
+void Edge::setNum(int newNum)
+{
+    m_num = newNum;
+}
+
+
+void Edge::afficher()
+{
+
+    std::cout << "num : " << getNum() << "  " << "name : " << getNom()<< "  " << "type : " << getType() << "  " << "start : " << getSource().getVertex()<< "  " << "end : " << getDest().getVertex() << "    weight : " << getWeight() <<std::endl;
+
+}
+
+
