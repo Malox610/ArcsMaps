26a65b8a582d2be17da2d3bf102cc8520d98b447
ESPEDUZA <75570746+ESPEDUZA@users.noreply.github.com>
2021-04-07 16:08:06 +0200

    Update node.h

--- node.h
+++ node.h
@@ -1,5 +1,6 @@
 #ifndef NODE_H_INCLUDED
 #define NODE_H_INCLUDED
+#include "string"
 
 class Node
 {
@@ -7,13 +8,20 @@ private :
 
     int m_vertex;
     int m_weight;
+    std::string m_name;
 
 
 public :
 
-    Node(int _vertex, int _weight);
+    Node(int _vertex,std::string _name, int _weight);
+    Node();
     int getVertex() const;
     int getWeight()const;
+    std::string getName()const;
+    void setVertex(int newVertex);
+    void setWeight(int newWeight);
+    void setName(std::string newName);
+    void afficher();
     ~Node();
 
 };
