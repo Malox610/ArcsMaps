b8971d7871775be24cb1f537f06bf3d0352aa8bd
Malox610 <75571264+Malox610@users.noreply.github.com>
2021-04-10 23:26:20 +0200

    Update Skieur.h

--- Skieur.h
+++ Skieur.h
@@ -9,7 +9,7 @@ private :
     std::string m_pseudo;
     std::string m_mdp;
     std::string m_param1 = "0";
-    std::string m_param2 = "O";
+    char m_param2 = 79;
 
 public :
 
@@ -17,11 +17,11 @@ public :
     std::string getPseudo();
     std::string getMdp();
     std::string getParam1();
-    std::string getParam2();
+    char getParam2();
     void setPseudo(std::string _pseudo);
     void setMdp(std::string _mdp);
     void setParam1(std::string newParam1);
-    void setParam2(std::string newParam2);
+    void setParam2(char newParam2);
     void afficher();
 
 };
