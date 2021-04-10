64c539456fda6f849094b2749f1979b734c5ffbe
ESPEDUZA <75570746+ESPEDUZA@users.noreply.github.com>
2021-04-10 02:42:23 +0200

    Update Skieur.h

--- Skieur.h
+++ Skieur.h
@@ -8,14 +8,20 @@ private :
 
     std::string m_pseudo;
     std::string m_mdp;
+    bool m_param1 = 0;
+    char m_param2;
 
 public :
 
 
     std::string getPseudo();
     std::string getMdp();
+    bool getParam1();
+    char getParam2();
     void setPseudo(std::string _pseudo);
     void setMdp(std::string _mdp);
+    void setParam1(bool newParam1);
+    void setParam2(char newParam2);
     void afficher();
 };
 
